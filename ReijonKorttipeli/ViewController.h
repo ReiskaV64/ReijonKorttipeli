943150409e4e717383ec0cda9c20fa1686cf2929
Reijo Vuohelainen <ReijoHD@Reijo-2.local>
2015-02-22 12:50:25 +0200

    Initial Commit

---
+++ ReijonKorttipeli/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReijonKorttipeli
+//
+//  Created by Reijo Vuohelainen on 22.2.2015.
+//  Copyright (c) 2015 Reijo Vuohelainen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReijonKorttipeli/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ReijonKorttipeli
+//
+//  Created by Reijo Vuohelainen on 22.2.2015.
+//  Copyright (c) 2015 Reijo Vuohelainen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
