bac7075659dbb9349a2743eb2bb5fb96418f598b
Reijo Vuohelainen <ReijoHD@Reijo-2.local>
2015-02-22 13:03:38 +0200

    siis storyboard jai pois

--- ReijonKorttipeli/PlayingCardDeck.h
+++ ReijonKorttipeli/PlayingCardDeck.h
@@ -1,15 +1,17 @@
 //
-//  ViewController.h
+//  PlayingCardDeck.h
 //  korttipakka
 //
 //  Created by Reijo Vuohelainen on 17.1.2015.
 //  Copyright (c) 2015 Reijo Vuohelainen. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+//#import <Foundation/Foundation.h>
+#import "Deck.h"
 
-@interface ViewController : UIViewController
+
+
+@interface PlayingCardDeck : Deck
 
 
 @end
-
